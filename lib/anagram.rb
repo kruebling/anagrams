852a22e0a856b05d25b2777530b8c936fe337fa5
kr <ktrhx4@mail.missouri.edu>
2017-09-08 15:05:48 -0700

    add palindrome functionality to word1

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,13 +1,18 @@
 #!/usr/bin/env ruby
 
-
-class Anagram
+class Words
   def anagrams(w1, w2)
     wordArr1 = w1.downcase.split(//).sort
     wordArr2 = w2.downcase.split(//).sort
-
       if (wordArr1 == wordArr2)
         return "these words are anagrams"
       end
+  end
+  def palindromes(w1, w2)
+    word_pal1 = w1.downcase
+    word_pal2 = w2.downcase
+    if word_pal1 == word_pal1.reverse
+      return "this word is a palindrome"
+    end
   end
 end
--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -1,8 +1,8 @@
 require('rspec')
 require('anagram')
 
-describe('Anagram#anagrams') do
-  ex = Anagram.new()
+describe('Words#anagrams') do
+  ex = Words.new()
 
   it('checks to see if two words are anagrams') do
     expect(ex.anagrams("hello", "olleh")).to(eq("these words are anagrams"))
@@ -10,6 +10,15 @@ describe('Anagram#anagrams') do
 
   it('checks to see if two words are anagrams even if their case is different') do
     expect(ex.anagrams("HeLlo", "oLlEh")).to(eq("these words are anagrams"))
+  end
+
+end
+
+describe('Words#palindromes') do
+  ex = Words.new()
+
+  it('checks to see if either of the words are palindromes') do
+    expect(ex.palindromes("Anna", "Anlo")).to(eq("this word is a palindrome"))
   end
 
 end
