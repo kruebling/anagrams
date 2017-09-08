acec95466c6e1f1f6670f694faa53bc0273b92ba
kr <ktrhx4@mail.missouri.edu>
2017-09-08 13:55:25 -0700

    check to see if two inputs are anagrams

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,8 +5,10 @@ class Anagram
   def anagrams(w1, w2)
     wordArr1 = w1.split(//).sort
     wordArr2 = w2.split(//).sort
-      if (w1 == w2)
+      if (wordArr1 == wordArr2)
         puts "these words are anagrams"
       end
+    wordArr1
+    wordArr2
   end
 end
--- lib/anagram_script.rb
+++ lib/anagram_script.rb
@@ -1,12 +1,11 @@
 #!/usr/bin/env ruby
 require_relative('./anagram')
-
 example = Anagram.new()
+
 puts "Please enter word"
 input1 = gets.chomp
 puts "Please enter a second word"
 input2 = gets.chomp
 
 result = example.anagrams(input1, input2)
-
 puts result
